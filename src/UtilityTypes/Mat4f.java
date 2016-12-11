1cc64b4ce558625f5cda2f929b808dcf3cefbf4f
Ted Gress <ted_gress@yahoo.com>
2016-12-11 15:35:13 -0500

    Workingon matrices

--- src/UtilityTypes/Mat4f.java
+++ src/UtilityTypes/Mat4f.java
@@ -8,6 +8,21 @@ public class Mat4f {
 	public float [][] matrix = new float[4][4];
 	
 	
+	//translate this matrix by the incoming matrix
+	public void translate() { }
+	
+	//scale this matrix by the incoming matrix
+	public void scale() { }
+	
+	//rotate this matrix by the incoming matrix
+	public void rotX() { }
+	
+	//rotate this matrix by the incoming matrix
+	public void rotY() { }
+	
+	//rotate this matrix by the incoming matrix
+	
+	public void rotz() { }
 	
 	
 	
